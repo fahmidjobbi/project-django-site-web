1f0c2af32844fe22c26de384ceb6d7959f0bd640
Fahmi Djobbi <73825072+fahmidjobbi@users.noreply.github.com>
2021-11-18 19:15:48 +0100

    WireFrame

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "bootstrap5",
+    'fontawesome_5',
     'store'
     
 ]
@@ -128,6 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 
 # Default settings
